248f33d94b630599d79a7eb4ccec3caba75a6a77
Davi Gomes <davigomes.ti.ads@gmail.com>
2021-11-24 08:24:31 -0300

    acrescentando os traços

--- Program/Program.cs
+++ Program/Program.cs
@@ -6,10 +6,12 @@ namespace Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("---------------------------------");
             Console.WriteLine("Hello World!");
             Console.WriteLine("Bom dia");
             Console.WriteLine("Como você está hoje?");
             Console.WriteLine("Tenha um bom dia");
+            Console.WriteLine("---------------------------------");
 
             Console.ReadLine();
 
