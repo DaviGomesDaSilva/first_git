cf3d87a9cc0ecff0d885670984bb6feb27b4d13e
Davi Gomes <davigomes.ti.ads@gmail.com>
2021-11-24 07:57:54 -0300

    acrescentado mensagem como vc esta hoje

--- Program/Program.cs
+++ Program/Program.cs
@@ -8,6 +8,7 @@ namespace Program
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Bom dia");
+            Console.WriteLine("Como você está hoje?");
 
             Console.ReadLine();
 
